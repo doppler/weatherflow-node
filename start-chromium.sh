153de224422d3b71d097d1a565a53e96ab666c56
David Rose <doppler@gmail.com>
2020-08-25 09:01:07 -0400

    decrease timeout to start chromium

--- start-chromium.sh
+++ start-chromium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! nc -q 1 localhost 5000 </dev/null; do sleep 10; done
+while ! nc -q 1 localhost 5000 </dev/null; do sleep 1; done
 
-/usr/bin/chromium --start-fullscreen http://localhost:5000
+/usr/bin/env chromium --start-fullscreen http://localhost:5000 & > /dev/null
 
