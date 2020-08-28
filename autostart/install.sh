d308a8313162cacce06e0836b6d6cd846b225ae8
David Rose <doppler@gmail.com>
2020-08-28 12:46:09 -0400

    added services to autostart

---
+++ autostart/install.sh
@@ -0,0 +1,9 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo $SCRIPTPATH
+sudo cp $SCRIPTPATH/weatherflow-dashboard.service /etc/systemd/system
+
+systemctl enable weatherflow-dashboard.service
+systemctl start weatherflow-dashboard.service
+
+sudo cp $SCRIPTPATH/autostart /etc/xdg/lxsession/LXDE-pi/autostart
