950486ca41bbf562f6a7b2418b8dd97cb1925daa
David Rose <doppler@gmail.com>
2020-08-28 12:49:30 -0400

    sudo-ify systemctl commands

--- autostart/install.sh
+++ autostart/install.sh
@@ -3,7 +3,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPTPATH
 sudo cp $SCRIPTPATH/weatherflow-dashboard.service /etc/systemd/system
 
-systemctl enable weatherflow-dashboard.service
-systemctl start weatherflow-dashboard.service
+sudo systemctl enable weatherflow-dashboard.service
+
+sudo systemctl start weatherflow-dashboard.service
 
 sudo cp $SCRIPTPATH/autostart /etc/xdg/lxsession/LXDE-pi/autostart
